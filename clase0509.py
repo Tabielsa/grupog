544eded024c57179b228e36fa16540cf38dbe25b
Tabielsa <anedcatt@gmail.com>
2022-09-20 20:21:48 -0300

    Ganador

---
+++ clase0509.py
@@ -0,0 +1,15 @@
+# Ejercicio 6 
+
+import secrets
+
+X = int(input("Ingrese un número:"))
+
+while X!=1:
+    if X%2==0:
+        X = int(X/2)
+    else:
+        X = 3*X+1
+    print (X)
+
+# Clase
+
